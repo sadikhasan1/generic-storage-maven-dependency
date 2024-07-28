4aa3739baf256a924fbee97b9f3f54dcd6a8d0f0
sadik.hasan@dsinnovators.com <sadik.hasan@dsinnovators.com>
2024-07-28 16:45:21 +0600

    first commit

---
+++ src/main/java/com/dsi/storage/s3/S3StorageService.java
@@ -0,0 +1,38 @@
+package com.dsi.storage.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.example.storage.core.StorageService;
+
+import java.io.InputStream;
+
+public class S3StorageService implements StorageService {
+
+    private final AmazonS3 amazonS3;
+    private final String bucketName;
+
+    public S3StorageService(AmazonS3 amazonS3, String bucketName) {
+        this.amazonS3 = amazonS3;
+        this.bucketName = bucketName;
+    }
+
+    @Override
+    public void upload(String objectName, InputStream data, String contentType) throws Exception {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(contentType);
+        amazonS3.putObject(new PutObjectRequest(bucketName, objectName, data, metadata));
+    }
+
+    @Override
+    public InputStream download(String objectName) throws Exception {
+        S3Object s3Object = amazonS3.getObject(bucketName, objectName);
+        return s3Object.getObjectContent();
+    }
+
+    @Override
+    public void delete(String objectName) throws Exception {
+        amazonS3.deleteObject(bucketName, objectName);
+    }
+}
