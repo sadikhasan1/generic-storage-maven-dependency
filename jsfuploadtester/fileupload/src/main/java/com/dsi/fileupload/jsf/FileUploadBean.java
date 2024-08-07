be63eb758d8ebffa612f1eeda0002422ee599a0f
Sadik Hasan <sadik.hasan@dynamicmalloc.xyz>
2024-08-07 10:36:37 +0600

    work in progress

--- jsfuploadtester/fileupload/src/main/java/com/dsi/fileupload/jsf/FileUploadBean.java
+++ jsfuploadtester/fileupload/src/main/java/com/dsi/fileupload/jsf/FileUploadBean.java
@@ -1,45 +1,42 @@
 package com.dsi.fileupload.jsf;
 
 import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import org.primefaces.model.file.UploadedFile;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import io.minio.errors.MinioException;
+import org.primefaces.model.file.UploadedFiles;
+import com.dsi.storage.core.StorageService;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 @Named
 @ViewScoped
 public class FileUploadBean implements Serializable {
 
-    private UploadedFile file;
+//    private final StorageService storageService;
+//
+//    public FileUploadBean() {
+//        this.storageService = StorageService.init();
+//    }
 
+    private UploadedFiles files;
 
-    public UploadedFile getFile() {
-        return file;
+    public UploadedFiles getFiles() {
+        return files;
     }
 
-    public void setFile(UploadedFile file) {
-        this.file = file;
+    public void setFiles(UploadedFiles files) {
+        this.files = files;
     }
 
     public void upload() {
-        if (file != null) {
-            try {
-                FileStorageService fileStorageService = new FileStorageService();
-                fileStorageService.upload(file.getFileName(), file.getInputStream(), file.getContentType());
-                System.out.println("Uploaded file: " + file.getFileName());
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println("Error uploading file: " + e.getMessage());
-            }
-        } else {
-            System.out.println("No file selected.");
-        }
+//        if (files != null && !files.getFiles().isEmpty()) {
+//            for (UploadedFile file : files.getFiles()) {
+//                storageService.upload("jsf/testing/nested/folder", file);
+//            }
+//        } else {
+//            System.out.println("No file selected.");
+//        }
+        System.out.println("No file selected.");
     }
 }
